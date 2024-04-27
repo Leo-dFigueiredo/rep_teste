b04a1fa1d209286165d3ed839834d9166b876c82
Leo_dFigueiredo <eng.leonardodefigueiredo@gmail.com>
2024-04-27 14:46:13 -0300

    Versão2

---
+++ Mod2_Aula3.py
@@ -0,0 +1,26 @@
+""" import time
+
+
+def PensarPor10Segundos():
+    
+
+    print('pensando')
+    time.sleep(10)
+    print('Lembrei!')
+
+if 10 > 5:
+    print('10 é maior que 5')
+
+class BemVindo():
+    def __init__(self):
+        print('Bem vindo')
+
+oi = BemVindo() """
+
+
+######## Modulo 2 - Aula 4 - Strings ########
+
+""" print ("Vamos codar!")
+print ("Vamos 'codar' ")
+print ("Vamos \n 'codar' ") """
+
